4454e3f9d7b245447a80f4b0c16b246ed847fd18
shorty-fr <jerome.demonchaux@gmail.com>
2015-09-26 20:58:33 +0200

    post episodes en attente

--- Views/assets/js/script.js
+++ Views/assets/js/script.js
@@ -2,59 +2,6 @@ $(document).ready(function () {
     $(document).on('click', ".btClear" ,function () {
         $(this).parent().parent().remove();
     });
-//
-//    $("#formUpload").on('submit', function (e) {
-//        e.preventDefault();
-//        $("#formUpload").hide("slow");
-//        $(".loader").show("slow");
-//        $(".determinate").attr("style", "width: 0%;");
-//
-//        //On recupère le nombre de video a envoyer
-//        var nbVideo = document.getElementById("uploadFiles").files.length;
-//        var current = 0;
-//
-//        //Fonction recursive qui envoie les vidéos une à une
-//        uploadVideo(current, nbVideo);
-//    });
-//
-//
-//});
-//
-//function uploadVideo(current, nbVideo) {
-//    var formData = new FormData();
-//    formData.append('videos', $("input[type=file]")[0].files[current]);
-//    $.ajax({
-//        url: "http://localhost:8080/upload",
-//        type: "POST",
-//        data: formData,
-//        //Les 3 options suivantes ont obligatoire pour le formData.
-//        cache: false,
-//        contentType: false,
-//        processData: false,
-//        //xhr: function () {
-//        //    var request = $.ajaxSettings.xhr();
-//        //    if (request.upload) { // Test si la propriété upload existe.
-//        //        request.addEventListener('progress', showProgress, false);
-//        //    }
-//        //    return request;
-//        //},
-//        success: function (msg) {
-//            var prog = parseInt( (current / nbVideo * 100), 10) + "%";
-//            $('.progressUpload').show("slow");
-//            $(".determinate").attr("style", "width: "+prog+";");
-//            current++;
-//            if (current <= nbVideo) {
-//                uploadVideo(current, nbVideo);
-//            } else {
-//                $(".loader").hide("slow");
-//                $("#formUpload").show("slow");
-//            }
-//        },
-//        error: function (err) {
-//            $(".loader").hide("slow");
-//            $("#formUpload").show("slow");
-//            $('.progressUpload').show("slow");
-//            $('.progressUpload').append("<p>error : " + err + "</p>");
-//        }
-//    })
+
+
 });
---
+++ Views/assets/js/toSort.js
@@ -0,0 +1,98 @@
+var selectAll = false;
+
+(function ($) {
+    $.each(['show', 'hide'], function (i, ev) {
+        var el = $.fn[ev];
+        $.fn[ev] = function () {
+            this.trigger(ev);
+            return el.apply(this, arguments);
+        };
+    });
+})(jQuery);
+
+/*
+ * Au chargement de la vue
+ */
+$("#atrier").on("show", function () {
+    $.ajax({
+        url: "/tosort",
+        type: "GET",
+        success: function (data) {
+            construct(data);
+        }
+    })
+});
+function construct(data) {
+    $("tbody").empty();
+    data = JSON.parse(data);
+    for (x in data) {
+        $("tbody").append('<tr><td><input type="checkbox" id="fichier' + x + '" /><label for="fichier' + x + '"></label></td><td>' + data[x] + '</td></tr>');
+    }
+}
+
+
+/*
+ * Checkbox selectAll dans header tableau
+ */
+$("#selectAll").on('click', function () {
+    if (selectAll == true) {
+        $('input[type=checkbox]').each(function () {
+            $(this).prop("checked", false);
+        });
+        selectAll = false;
+    } else {
+        $('input[type=checkbox]').each(function () {
+            $(this).prop("checked", true);
+        });
+        selectAll = true;
+    }
+});
+
+
+/**
+ * Bouton ajouter serie
+ */
+$("#addSerie").on('click', function () {
+    // Vérif d'erreur a l'arrache
+    var files = false;
+    $('input[type=checkbox]').each(function () {
+        if ($(this).prop('checked')) {
+            files = true;
+        }
+    });
+
+    if ($("#nomSerie").val() == "") {
+        files = false;
+    }
+
+
+    if (files) {
+        var nomSerie = $("#nomSerie").val();
+        var data = {name: nomSerie};
+        var episodes = [];
+        $('input[type=checkbox]').each(function () {
+            if ($(this).prop('checked')) {
+                //Recup le nom de l'episode
+                var episode = $(this).closest("td").next().text();
+                //push ds tableau
+                episodes.push(episode);
+            }
+        });
+        data.episodes = episodes;
+        $.ajax({
+            url:"/topost",
+            type: "post",
+            data: "serie="+JSON.stringify(data),
+            success: function(data) {
+                console.log("post ok");
+            },
+            error: function(data) {
+                console.log("error Post");
+            }
+
+        })
+    }
+    else {
+        console.log("aucune serie");
+    }
+});
